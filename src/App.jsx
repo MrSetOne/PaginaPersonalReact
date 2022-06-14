6708d9b52c98b93d7e549b66383715e3fa1a6dfc
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-14 21:18:47 +0200

    passport

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Aboutme from './components/Aboutme/Aboutme';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/Navbar/Navbar';
+import Portfolio from './components/Portfolio/Portfolio';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Aboutme/>}/>
           <Route path="/contact" element={<Contact/>}/>
+          <Route path="/portfolio" element={<Portfolio/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,47 @@
+const Portfolio = () => {
+    const portfolio =[
+        {
+            github:"https://github.com/MrSetOne/04.BlogDeCafe",
+            title:"Blog de Café"
+        },
+        {
+            github:"https://github.com/MrSetOne/PaginaPersonal",
+            title:"Pagina personal"
+        },
+        {
+            github:"https://github.com/MrSetOne/TB_10_FakeTwitter",
+            title: "Clon de Twitter"
+        },
+        {
+            github:"https://github.com/MrSetOne/TB_25_FakeInstagram",
+            title:"Clon de Instagram"
+        },
+        {
+            github:"https://github.com/MrSetOne/TaskR",
+            title: "Taskr"
+        },
+        {
+            github:"https://github.com/MrSetOne/Quiz",
+            title: "GameQuiz!"
+        },
+        {
+            github:"https://github.com/MrSetOne/Frikishop",
+            title: "Frikishop"
+        },
+        {
+            github:"https://github.com/MrSetOne/SocialNetwork_MongoDB",
+            title: "Red social"
+        }
+    ]
+  
+  
+  
+  
+    return (
+        <section className="Portfolio">
+            <p>holi</p>
+        </section>
+    )
+}
+
+export default Portfolio
