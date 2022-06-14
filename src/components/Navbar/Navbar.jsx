76000fc84697f9f2f01ae1abeacd47cc5ce1ccf1
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-14 13:04:39 +0200

    init

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar__container">
+        <h2 className="navbar__title">&lt;Michael Lara/&gt;</h2>
+    </nav>
+  )
+}
+
+export default Navbar
