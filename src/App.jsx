14fd37d963f3a6a658c74ba1e4feb368184966f7
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-14 15:14:41 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import 'normalize.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Aboutme from './components/Aboutme/Aboutme';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <header>
           <Navbar/>
-          <Contact/>
-          <Aboutme/>
       </header>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Aboutme/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+          </BrowserRouter>
     </div>
   );
 }
